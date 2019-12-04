04d4ea6113a0cfcabec33828754323c1a9d33e2a
Agres <AgressoRj@gmail.com>
2019-12-04 22:38:55 +0300

    init

---
+++ src/main/java/ru/starokozhev/SocialManager/SocialManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.starokozhev.SocialManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/starokozhev/SocialManager/SocialManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.starokozhev.SocialManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
