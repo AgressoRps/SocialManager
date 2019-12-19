6d9e0fefc20c2a89173dd29fdd8bf0533b19ab16
Agres <AgressoRj@gmail.com>
2019-12-19 21:17:54 +0300

    delegate 404 to frontend

---
+++ src/main/java/ru/starokozhev/SocialManager/controller/MainController.java
@@ -0,0 +1,21 @@
+package ru.starokozhev.SocialManager.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@Controller
+@ControllerAdvice
+public class MainController {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public String handleResourceNotFoundException() {
+        return "index.html";
+    }
+
+}
