71a92bdb6d399e302992a6799d949912ffe51e0c
Agres <AgressoRj@gmail.com>
2020-01-25 21:03:30 +0300

    add empty methods in services

--- src/main/java/ru/starokozhev/SocialManager/service/ProxyService.java
+++ src/main/java/ru/starokozhev/SocialManager/service/ProxyService.java
@@ -2,6 +2,9 @@ package ru.starokozhev.SocialManager.service;
 
 import org.springframework.stereotype.Service;
 import ru.starokozhev.SocialManager.dto.ProxyWrapper;
+import ru.starokozhev.SocialManager.dto.filter.ProxyFilter;
+
+import java.util.List;
 
 @Service
 public class ProxyService {
@@ -10,4 +13,19 @@ public class ProxyService {
         return null;
     }
 
+    public ProxyWrapper edit(ProxyWrapper wrapper) {
+        return null;
+    }
+
+    public ProxyWrapper get(String key) {
+        return null;
+    }
+
+    public void delete(String key) {
+
+    }
+
+    public List<ProxyWrapper> list(ProxyFilter filter) {
+        return null;
+    }
 }
--- src/main/java/ru/starokozhev/SocialManager/service/StrategyService.java
+++ src/main/java/ru/starokozhev/SocialManager/service/StrategyService.java
@@ -1,7 +1,38 @@
 package ru.starokozhev.SocialManager.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.starokozhev.SocialManager.dto.StrategyWrapper;
+import ru.starokozhev.SocialManager.dto.filter.StrategyFilter;
+import ru.starokozhev.SocialManager.entity.Strategy;
+import ru.starokozhev.SocialManager.repository.StrategyRepository;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class StrategyService {
+
+    private final StrategyRepository strategyRepository;
+
+    public StrategyWrapper add(StrategyWrapper wrapper) {
+        return null;
+    }
+
+    public StrategyWrapper edit(StrategyWrapper wrapper) {
+        return null;
+    }
+
+    public StrategyWrapper get(String name) {
+        return null;
+    }
+
+    public void delete(String name) {
+
+    }
+
+    public List<StrategyWrapper> list(StrategyFilter filter) {
+        return null;
+    }
+
 }
