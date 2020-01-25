4c946b7bec9e3cb257a22ddc5a896adbf40cf16d
Agres <AgressoRj@gmail.com>
2020-01-25 17:45:17 +0300

    add empty methods in services

--- src/main/java/ru/starokozhev/SocialManager/service/EarnedService.java
+++ src/main/java/ru/starokozhev/SocialManager/service/EarnedService.java
@@ -3,10 +3,27 @@ package ru.starokozhev.SocialManager.service;
 import org.springframework.stereotype.Service;
 import ru.starokozhev.SocialManager.dto.EarnedWrapper;
 
+import java.util.List;
+
 @Service
 public class EarnedService {
 
     public EarnedWrapper add(EarnedWrapper wrapper) {
+        return null;
+    }
+
+    public EarnedWrapper edit(EarnedWrapper wrapper) {
+        return null;
+    }
+
+    public EarnedWrapper get(Long id) {
+        return null;
+    }
+
+    public void delete(Long id) {
+    }
+
+    public List<EarnedWrapper> list() {
         return null;
     }
 
--- src/main/java/ru/starokozhev/SocialManager/service/MailService.java
+++ src/main/java/ru/starokozhev/SocialManager/service/MailService.java
@@ -1,7 +1,37 @@
 package ru.starokozhev.SocialManager.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.starokozhev.SocialManager.dto.MailWrapper;
+import ru.starokozhev.SocialManager.dto.filter.MailFilter;
+import ru.starokozhev.SocialManager.repository.MailRepository;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MailService {
+
+    private final MailRepository mailRepository;
+
+    public MailWrapper add(MailWrapper wrapper) {
+        return null;
+    }
+
+    public MailWrapper edit(MailWrapper wrapper) {
+        return null;
+    }
+
+    public MailWrapper get(String mail) {
+        return null;
+    }
+
+    public void delete(String mail) {
+
+    }
+
+    public List<MailWrapper> list(MailFilter filter) {
+        return null;
+    }
+
 }
