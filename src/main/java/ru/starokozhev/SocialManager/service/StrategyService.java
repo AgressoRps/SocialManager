c8c7445fd0d4a3a5c20294017be7674ff6380a8d
Agres <AgressoRj@gmail.com>
2020-01-26 23:04:05 +0300

    update StrategyService

--- src/main/java/ru/starokozhev/SocialManager/service/StrategyService.java
+++ src/main/java/ru/starokozhev/SocialManager/service/StrategyService.java
@@ -3,8 +3,10 @@ package ru.starokozhev.SocialManager.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.starokozhev.SocialManager.dto.StrategyWrapper;
-import ru.starokozhev.SocialManager.dto.filter.StrategyFilter;
+import ru.starokozhev.SocialManager.entity.Bot;
 import ru.starokozhev.SocialManager.entity.Strategy;
+import ru.starokozhev.SocialManager.entity.User;
+import ru.starokozhev.SocialManager.repository.BotRepository;
 import ru.starokozhev.SocialManager.repository.StrategyRepository;
 
 import java.util.List;
@@ -14,12 +16,34 @@ import java.util.List;
 public class StrategyService {
 
     private final StrategyRepository strategyRepository;
+    private final BotRepository botRepository;
+    private final UserService userService;
 
     public StrategyWrapper add(StrategyWrapper wrapper) {
-        return null;
+        User user = userService.getCurrentUser();
+        Strategy strategy = new Strategy();
+
+        Strategy existStrategy = strategyRepository.findStrategyByNameAndUser(wrapper.getName(), user);
+        if (existStrategy != null)
+            throw new IllegalArgumentException("Стратегия с таким именем уже существует!");
+
+        Bot bot = botRepository.findByNameAndUser(wrapper.getBot().getName(), user);
+        if (bot == null)
+            throw new IllegalArgumentException("Бот не найден!");
+
+        wrapper.fromWrapper(strategy);
+        strategy.setBot(bot);
+        strategy.setUser(user);
+
+        return new StrategyWrapper(strategyRepository.save(strategy));
     }
 
     public StrategyWrapper edit(StrategyWrapper wrapper) {
+        User user = userService.getCurrentUser();
+        Strategy strategy = strategyRepository.findStrategyByNameAndUser(wrapper.getName(), user);
+        //TODO TODO TODO
+
+
         return null;
     }
 
@@ -31,7 +55,7 @@ public class StrategyService {
 
     }
 
-    public List<StrategyWrapper> list(StrategyFilter filter) {
+    public List<StrategyWrapper> list() {
         return null;
     }
 
